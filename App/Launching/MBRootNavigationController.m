3699535c6d8909a21eacdf1355182d80c9bcd895
BB9z <bb9z@me.com>
2014-08-06 16:23:27 +0800

    - MBRootNavigationController 多余的调用

--- App/Launching/MBRootNavigationController.m
+++ App/Launching/MBRootNavigationController.m
@@ -7,9 +7,6 @@
 @implementation MBRootNavigationController
 
 - (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    if ([self.forwardDelegate respondsToSelector:@selector(navigationController:willShowViewController:animated:)]) {
-        [self.forwardDelegate navigationController:navigationController willShowViewController:viewController animated:animated];
-    }
 
     // 修正 iOS 6 下与按钮位置与 iOS 7 的偏差
     if (RF_iOS7Before) {
