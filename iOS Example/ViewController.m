2d1952bddfc459ac18ec9cc1b03e6ae3307ab107
BB9z <bb9z@me.com>
2016-01-16 14:51:55 +0800

    添加空的示例项目

---
+++ iOS Example/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOS Example
+//
+//  Created by BB9z on 1/16/16.
+//  Copyright © 2016 Beijing ZhiYun ZhiYuan Technology Co., Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iOS Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS Example
+//
+//  Created by BB9z on 1/16/16.
+//  Copyright © 2016 Beijing ZhiYun ZhiYuan Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
