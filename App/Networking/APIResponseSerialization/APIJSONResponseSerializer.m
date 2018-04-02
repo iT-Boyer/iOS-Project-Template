7d78f4801523b5e5437b300152df75ad963583cb
BB9z <bb9z@me.com>
2018-04-02 21:13:19 +0800

    APIJSONResponseSerializer, simplfy error handling.

--- App/Networking/APIResponseSerialization/APIJSONResponseSerializer.m
+++ App/Networking/APIResponseSerialization/APIJSONResponseSerializer.m
@@ -36,6 +36,28 @@ extern NSString *const APIErrorDomain;
     return self;
 }
 
+/// reason, suggestion 传 @"" 用默认值
+- (void)makeError:(NSError *__autoreleasing *__nullable)error withDebugMessage:(nullable NSString *)debugMessage domain:(NSErrorDomain)domain code:(NSInteger)code description:(nonnull NSString *)description reason:(nullable NSString *)reason suggestion:(nullable NSString *)suggestion url:(NSURL *)url {
+#if RFDEBUG
+    if (debugMessage) {
+        dout_error(@"%@", debugMessage);
+    }
+#endif
+    if (!error) return;
+    if (reason && reason.length == 0) {
+        reason = @"可能服务器正在升级或者维护，也可能是应用bug";
+    }
+    if (suggestion && suggestion.length == 0) {
+        suggestion = @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本";
+    }
+    NSMutableDictionary *info = [NSMutableDictionary dictionaryWithCapacity:4];
+    [info rf_setObject:description forKey:NSLocalizedDescriptionKey];
+    [info rf_setObject:reason forKey:NSLocalizedFailureReasonErrorKey];
+    [info rf_setObject:suggestion forKey:NSLocalizedRecoverySuggestionErrorKey];
+    [info rf_setObject:url forKey:NSURLErrorFailingURLErrorKey];
+    *error = [NSError errorWithDomain:domain code:code userInfo:info];
+}
+
 // NSHTTPURLResponse 的验证，在此验证 header 和 status code 比较好
 // responseObjectForResponse:data:error: 做其它验证
 - (BOOL)validateResponse:(NSHTTPURLResponse *)response data:(NSData *)data error:(NSError *__autoreleasing *)error {
@@ -50,56 +72,30 @@ extern NSString *const APIErrorDomain;
             // 服务器通过 status code 报错
         }
         else {
-            if (error) {
 #if RFDEBUG
-                NSString *errorMessage = [NSString stringWithFormat:@"请求状态异常：%@ (%ld)\n\
+            NSString *errorMessage = [NSString stringWithFormat:@"请求状态异常：%@ (%ld)\n\
 HTTP 状态与 ResponseSerializer 的 acceptableStatusCodes 预期不符合\n\
 如果服务器通过 HTTP 状态报错，请增加 acceptableStatusCodes 的设置", [NSHTTPURLResponse localizedStringForStatusCode:response.statusCode], (long)response.statusCode];
-                dout_error(@"%@", errorMessage);
+            dout_error(@"%@", errorMessage);
 #endif
-
-                *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorBadServerResponse userInfo:@{
-                    NSLocalizedDescriptionKey : [NSString stringWithFormat:@"请求状态异常：%@ (%ld)", [NSHTTPURLResponse localizedStringForStatusCode:response.statusCode], (long)response.statusCode],
-                    NSLocalizedFailureReasonErrorKey : @"可能服务器正在升级或者维护，也可能是应用bug",
-                    NSLocalizedRecoverySuggestionErrorKey : @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本",
-                    NSURLErrorFailingURLErrorKey : response.URL
-                }];
-            }
+            [self makeError:error withDebugMessage:nil domain:NSURLErrorDomain code:NSURLErrorBadServerResponse description:[NSString stringWithFormat:@"请求状态异常：%@ (%ld)", [NSHTTPURLResponse localizedStringForStatusCode:response.statusCode], (long)response.statusCode] reason:@"" suggestion:@"" url:response.URL];
             return NO;
         }
     }
 
     // HTTP Content-Type 检查
     if (self.acceptableContentTypes && ![self.acceptableContentTypes containsObject:response.MIMEType]) {
-        if (error) {
 #if RFDEBUG
-            NSString *errorMessage = [NSString stringWithFormat:@"服务器返回的 Content-Type 是 %@，而可以接受的是 %@\n请联系后台人员，把 JSON 返回的 Content-Type 改成标准的 application/json", response.MIMEType, self.acceptableContentTypes];
-            dout_error(@"%@", errorMessage);
+        NSString *errorMessage = [NSString stringWithFormat:@"服务器返回的 Content-Type 是 %@，而可以接受的是 %@\n请联系后台人员，把 JSON 返回的 Content-Type 改成标准的 application/json", response.MIMEType, self.acceptableContentTypes];
+        dout_error(@"%@", errorMessage);
 #endif
-            *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorBadServerResponse userInfo:@{
-                NSLocalizedDescriptionKey : @"服务器返回的类型与预期不符合",
-                NSLocalizedFailureReasonErrorKey : @"可能服务器正在升级或者维护，也可能是应用bug",
-                NSLocalizedRecoverySuggestionErrorKey : @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本",
-                NSURLErrorFailingURLErrorKey : response.URL
-            }];
-        }
+        [self makeError:error withDebugMessage:nil domain:NSURLErrorDomain code:NSURLErrorBadServerResponse description:@"服务器返回的类型与预期不符合" reason:@"" suggestion:@"" url:response.URL];
         return NO;
     }
 
     // 非空检查
     if (!data.length) {
-        if (error) {
-#if RFDEBUG
-            NSString *errorMessage = [NSString stringWithFormat:@"空内容不被视为正常返回\n请联系后台人员确认状况"];
-            dout_error(@"%@", errorMessage);
-#endif
-            *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorZeroByteResource userInfo:@{
-                NSLocalizedDescriptionKey : @"服务器返回空的内容",
-                NSLocalizedFailureReasonErrorKey : @"可能服务器正在升级或者维护，也可能是应用bug",
-                NSLocalizedRecoverySuggestionErrorKey : @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本",
-                NSURLErrorFailingURLErrorKey : response.URL
-            }];
-        }
+        [self makeError:error withDebugMessage:@"空内容不被视为正常返回\n请联系后台人员确认状况" domain:NSURLErrorDomain code:NSURLErrorZeroByteResource description:@"服务器返回空的内容" reason:@"" suggestion:@"" url:response.URL];
         return NO;
     }
 
@@ -118,18 +114,7 @@ HTTP 状态与 ResponseSerializer 的 acceptableStatusCodes 预期不符合\n\
 
     NSString *responseString = [[NSString alloc] initWithData:data encoding:self.stringEncoding];
     if (!responseString.length) {
-        if (error) {
-#if RFDEBUG
-            NSString *errorMessage = [NSString stringWithFormat:@"因编码问题不能处理响应数据\n建议先验证返回是否是合法的JSON，并联系后台人员"];
-            dout_error(@"%@", errorMessage);
-#endif
-            *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorCannotDecodeContentData userInfo:@{
-                NSLocalizedDescriptionKey : @"无法处理服务器的返回",
-                NSLocalizedFailureReasonErrorKey : @"可能服务器正在升级或者维护，也可能是应用bug",
-                NSLocalizedRecoverySuggestionErrorKey : @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本",
-                NSURLErrorFailingURLErrorKey : response.URL
-            }];
-        }
+        [self makeError:error withDebugMessage:@"因编码问题不能处理响应数据\n建议先验证返回是否是合法的JSON，并联系后台人员" domain:NSURLErrorDomain code:NSURLErrorCannotDecodeContentData description:@"无法处理服务器的返回" reason:@"" suggestion:@"" url:response.URL];
         return nil;
     }
 
@@ -138,18 +123,8 @@ HTTP 状态与 ResponseSerializer 的 acceptableStatusCodes 预期不符合\n\
 
     // 检查能否解析
     if (e) {
-        if (error) {
-#if RFDEBUG
-            NSString *errorMessage = [NSString stringWithFormat:@"解析器返回的错误信息：%@\n建议先验证返回是否是合法的JSON，并联系后台人员", e.localizedDescription];
-            dout_error(@"%@", errorMessage);
-#endif
-            *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorCannotParseResponse userInfo:@{
-                NSLocalizedDescriptionKey : @"无法解析返回数据",
-                NSLocalizedFailureReasonErrorKey : @"可能服务器正在升级或者维护，也可能是应用bug",
-                NSLocalizedRecoverySuggestionErrorKey : @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本",
-                NSURLErrorFailingURLErrorKey : response.URL
-            }];
-        }
+        NSString *errorMessage = [NSString stringWithFormat:@"解析器返回的错误信息：%@\n建议先验证返回是否是合法的JSON，并联系后台人员", e.localizedDescription];
+        [self makeError:error withDebugMessage:errorMessage domain:NSURLErrorDomain code:NSURLErrorCannotParseResponse description:@"无法解析返回数据" reason:@"" suggestion:@"" url:response.URL];
         return nil;
     }
 
@@ -157,20 +132,11 @@ HTTP 状态与 ResponseSerializer 的 acceptableStatusCodes 预期不符合\n\
     if ([responseObject isKindOfClass:[NSDictionary class]]) {
         APIJSONError *APIError = [[APIJSONError alloc] initWithDictionary:responseObject error:nil];
         if (APIError) {
-            if (error) {
-#if RFDEBUG
-                NSString *errorMessage = [NSString stringWithFormat:@"服务器返回的错误信息：%@\n", [APIError localizedDescription]];
-                dout_error(@"%@", errorMessage);
-#endif
-                *error = [NSError errorWithDomain:APIErrorDomain code:APIError.errorCode userInfo:@{
-                    NSLocalizedDescriptionKey : APIError.errorDescription,
-                    NSURLErrorFailingURLErrorKey : response.URL
-                }];
-            }
+            [self makeError:error withDebugMessage:[NSString stringWithFormat:@"服务器返回的错误信息：%@\n", [APIError localizedDescription]] domain:APIErrorDomain code:APIError.errorCode description:APIError.errorDescription reason:nil suggestion:nil url:response.URL];
             return nil;
         }
     }
-
+    
     return responseObject;
 }
 
