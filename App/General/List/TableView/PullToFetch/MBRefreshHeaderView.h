df1f3f18112d15c1e25985b458b6f2d321684fd1
BB9z <bb9z@me.com>
2018-08-28 10:02:09 +0800

    MBRefreshHeaderView, restore activityIndicatorView.

--- App/General/List/TableView/PullToFetch/MBRefreshHeaderView.h
+++ App/General/List/TableView/PullToFetch/MBRefreshHeaderView.h
@@ -14,7 +14,7 @@
 @property (weak, nonatomic) IBOutlet UIView *contentView;
 @property (weak, nonatomic) IBOutlet UILabel *statusLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *indicatorImageView;
-@property (weak, nonatomic) IBOutlet UIImageView *activityIndicatorView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
 
 /**
  外部的空内容指示视图，如果设置 MBRefreshFooterView 会根据自身 empty 属性变化设置该视图的显隐
