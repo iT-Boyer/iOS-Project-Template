f232e17f10edbac6fb86948bbd83183d2357c4d6
BB9z <bb9z@me.com>
2018-10-19 17:20:49 +0800

    Date+App.swift enhanced.

--- App/General/Category/Date+App.swift
+++ App/General/Category/Date+App.swift
@@ -22,15 +22,51 @@ extension Date {
         return (self as NSDate).recentString
     }
     
-    /// 后台专用日期格式 yyyy-MM-dd
+    /// 后台专用日期格式
     var dayIdentifier: MBDateDayIdentifier {
         return DateFormatter.cachedDayIdentifier().string(from: self) as MBDateDayIdentifier
+    }
+    
+    /**
+     按当前历法判断两个可选日期在某一时间维度上是否一致
+     
+     都为 nil 会返回 true；2000-01-01 和 2000-05-01 在 .day，包括时分秒的维度上都是不一致的
+     
+     - parameter granularity: 比较的维度；比较的方式是纬度从大到小，指定维度以上都一样就算是一致；所以不会有传多个维度的场景
+     */
+    static func isSame(granularity: Calendar.Component, _ date1: Date?, _ date2: Date?) -> Bool {
+        if date1 == nil && date2 == nil {
+            return true
+        }
+        guard let date1 = date1, let date2 = date2 else {
+            return false
+        }
+        return Calendar.current.compare(date1, to: date2, toGranularity: granularity) == .orderedSame
     }
 }
 
 extension TimeInterval {
+    /// XX:XX 时长显示，超过一小时不显示小时
     var mmssString: String {
         let value = Int(self.rounded())
         return String(format: "%02d:%02d", value/60,  value%60)
+    }
+    
+    /**
+     按时长返回时分秒
+     
+     例
+     ```
+     print(TimeInterval(12345).durationComponents)
+     // (hour: 3, minute: 25, second: 45)
+     ```
+     */
+    var durationComponents: (hour: Int, minute: Int, second: Int) {
+        var second = Int(self.rounded())
+        let hour = second / 3600
+        second -= hour * 3600
+        let minute = second / 60
+        second -= minute * 60
+        return (hour, minute, second)
     }
 }
