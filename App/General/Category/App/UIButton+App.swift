12c02d28c82abfdb0b96f374977048f430e40535
BB9z <bb9z@me.com>
2018-11-28 10:37:25 +0800

    UIButton+App, text setter improved.

--- App/General/Category/App/UIButton+App.swift
+++ App/General/Category/App/UIButton+App.swift
@@ -8,13 +8,20 @@ extension UIButton {
      便捷读取/设置当前状态的文字
      
      为了和 UILabel 等控件的设置方式一致
+     
+     注意：设置时如果当前状态的文字未设置，则修改默认状态的文字；这么做符合多数情况下的意图
      */
     @objc var text: String? {
         get {
             return currentTitle
         }
         set {
-            setTitle(text, for: state)
+            if title(for: state) != nil {
+                setTitle(text, for: state)
+            }
+            else {
+                setTitle(text, for: .normal)
+            }
         }
     }
 }
