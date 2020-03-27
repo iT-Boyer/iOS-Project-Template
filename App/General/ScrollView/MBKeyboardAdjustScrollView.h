cbd137aa3a7d7d705c47e93850088089f6e4ffe6
BB9z <bb9z@me.com>
2020-03-27 22:12:20 +0800

    MBKeyboardAdjustScrollView, new arrival.

---
+++ App/General/ScrollView/MBKeyboardAdjustScrollView.h
@@ -0,0 +1,17 @@
+/*!
+ MBKeyboardAdjustScrollView
+
+ Copyright © 2020 RFUI.
+ https://github.com/BB9z/iOS-Project-Template
+
+ Apache License, Version 2.0
+ http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#import <RFKit/RFRuntime.h>
+
+/**
+ 随键盘调整 content inset
+ */
+@interface MBKeyboardAdjustScrollView : UIScrollView
+@end
