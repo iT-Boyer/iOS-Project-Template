3e589b1b4a5e281d57d3153b8ee612e7504ae8bb
BB9z <bb9z@me.com>
2020-07-22 19:33:19 +0800

    MBCollectionViewFlowLayout, clean iOS 9 patch.

--- App/General/List/CollectionView/Layout/MBCollectionViewFlowLayout.h
+++ App/General/List/CollectionView/Layout/MBCollectionViewFlowLayout.h
@@ -1,7 +1,7 @@
 /*
  MBCollectionViewFlowLayout
  
- Copyright © 2018 RFUI.
+ Copyright © 2018, 2020 RFUI.
  Copyright © 2014 Beijing ZhiYun ZhiYuan Information Technology Co., Ltd.
  https://github.com/BB9z/iOS-Project-Template
  
@@ -11,9 +11,11 @@
 #import <RFKit/RFRuntime.h>
 #import <RFInitializing/RFInitializing.h>
 
-// @MBDependency:4
+// @MBDependency:3
 /**
  UICollectionViewFlowLayout 等价体，修正特定版本系统的 bug
+
+ 当前已无任何修正
  */
 @interface MBCollectionViewFlowLayout : UICollectionViewFlowLayout <
     RFInitializing
--- App/General/List/CollectionView/Layout/MBCollectionViewFlowLayout.m
+++ App/General/List/CollectionView/Layout/MBCollectionViewFlowLayout.m
@@ -10,11 +10,4 @@ RFInitializingRootForNSObjectSupportNSCoding
 - (void)afterInit {
 }
 
-// @bug(iOS 9): 尝试解决 iOS 9 上的 _createPreparedSupplementaryViewForElementOfKind 异常
-// ref: http://stackoverflow.com/a/32823057/945906
-- (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
-    if (!self.collectionView.dataSource ) return nil;
-    return [super layoutAttributesForElementsInRect:rect];
-}
-
 @end
