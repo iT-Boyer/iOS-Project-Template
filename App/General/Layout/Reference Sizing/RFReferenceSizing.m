9ddffe0a3612eea05186ede7bd52666ce48b4284
BB9z <bb9z@me.com>
2020-07-27 23:00:54 +0800

    ReferenceSizing, 因需求小众移除

--- App/General/Layout/Reference Sizing/RFReferenceSizing.m
+++
@@ -1,7 +0,0 @@
-
-#import "RFReferenceSizing.h"
-
-double RFReferenceSizeRate(CGFloat referenceSize, CGFloat constant, CGFloat factor) {
-    NSCParameterAssert(referenceSize != 0);
-    return (constant / referenceSize - 1) * (factor?: 1) + 1;
-}
