d8865ab8d1d98c557461db215971d2b249b03886
BB9z <bb9z@me.com>
2020-07-27 22:46:19 +0800

    MBVauleLabel, nullable 声明

--- App/General/Text/MBVauleLabel.h
+++ App/General/Text/MBVauleLabel.h
@@ -1,7 +1,7 @@
 /*
  MBVauleLabel
  
- Copyright © 2018 RFUI.
+ Copyright © 2018, 2020 RFUI.
  Copyright © 2015-2016 Beijing ZhiYun ZhiYuan Information Technology Co., Ltd.
  https://github.com/BB9z/iOS-Project-Template
  
@@ -13,32 +13,35 @@
 
 // @MBDependency:1
 /**
- 设置
+ 设置 value，格式化显示
  */
 @interface MBVauleLabel : UILabel
-@property (strong, nonatomic) id value;
+@property (nullable, nonatomic) id value;
 
 /// 重写或设置 block 改变展示方式
-- (NSString *)displayStringForVaule:(id)value;
-@property (copy, nonatomic) NSString* (^vauleFormatBlock)(MBVauleLabel *label, id value);
+- (nullable NSString *)displayStringForVaule:(nullable id)value;
+@property (nullable) NSString*__nullable (^vauleFormatBlock)(__kindof MBVauleLabel *__nonnull label, id __nullable value);
 @end
 
 // @MBDependency:2
+/**
+ 支持占位符替换数值的富文本
+ */
 @interface MBVauleAttributedLabel : UILabel <
     RFInitializing
 >
-@property (strong, nonatomic) id value;
+@property (nullable, nonatomic) id value;
 
 /// 空值时显示的文本
-@property (nonatomic, copy) IBInspectable NSString *nullValueDisplayString;
+@property (nullable) IBInspectable NSString *nullValueDisplayString;
 
 /// 空数字显示空值文本
-@property (nonatomic) IBInspectable BOOL treatZeroNumberAsNull;
+@property IBInspectable BOOL treatZeroNumberAsNull;
 
 /// awakeFromNib 时设置，代码创建需要手动赋值
-@property (nonatomic, strong) NSAttributedString *attributedFormatString;
+@property (nullable) NSAttributedString *attributedFormatString;
 
 /// 重写或设置 block 改变展示方式
-- (NSAttributedString *)displayAttributedStringForVaule:(id)value;
-@property (copy, nonatomic) NSAttributedString* (^vauleFormatBlock)(MBVauleAttributedLabel *label, id value);
+- (nullable NSAttributedString *)displayAttributedStringForVaule:(nullable id)value;
+@property (nullable) NSAttributedString*__nullable (^vauleFormatBlock)(__kindof MBVauleAttributedLabel *__nonnull label, id __nullable value);
 @end
