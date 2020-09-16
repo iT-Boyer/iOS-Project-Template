10c2a33aa41ceb85fd5cfa7769bf97343a2292f7
BB9z <bb9z@me.com>
2020-09-16 16:19:44 +0800

    String+App, 新增号码打码方法

--- App/General/Extensions/Foundation/String+App.swift
+++ App/General/Extensions/Foundation/String+App.swift
@@ -32,4 +32,21 @@ extension String {
     func matches(regularExpression pattern: String) -> Bool {
         return range(of: pattern, options: .regularExpression) != nil
     }
+
+    /// 手机号/电话号打码
+    /// 支持任意长度
+    func phoneMasked(_ mask: Character = Character("*")) -> String {
+        guard !isEmpty else { return self }
+        // 用系数算可以支持任意长度的输入
+        // 系数是可以计算的，但这里写死的可读性好
+        let length = Double(count)
+        let maskCount = Int((length / 11.0 * 4.0).rounded())
+        guard maskCount > 0 else { return self }
+        let maskString = String(repeating: mask, count: maskCount)
+        let rangeStart = index(startIndex, offsetBy: Int((length * 0.29).rounded()))
+        let rangeEnd = index(rangeStart, offsetBy: maskCount - 1)
+        var str = self
+        str.replaceSubrange(rangeStart...rangeEnd, with: maskString)
+        return str
+    }
 }
